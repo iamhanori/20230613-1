9dde59f6c819f44adae2507ac0b0f9d6febac22e
Hanul Kim <s2122@e-mirim.hs.kr>
2023-06-13 14:48:25 +0900

    test1에 접속했을 때 result.html를 실행 + id, pass 콘솔창에 출력

--- src/main/java/kr/hs/study/Model2/controller/TestController.java
+++ src/main/java/kr/hs/study/Model2/controller/TestController.java
@@ -6,4 +6,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
+    // test1에 접속했을 때 result.html를 실행
+    @GetMapping("/test1")
+    public String text1(@RequestParam("id") String id,
+                        @RequestParam("pass") String pass) {
+        System.out.println(id);
+        System.out.println(pass);
+
+        return "result";
+    }
 }
