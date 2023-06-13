2b899777d3ac722ef5c1bc2e29d9bb8a402a6754
Hanul Kim <s2122@e-mirim.hs.kr>
2023-06-13 14:47:11 +0900

    Initial commit

---
+++ src/main/java/kr/hs/study/Model2/controller/TestController.java
@@ -0,0 +1,9 @@
+package kr.hs.study.Model2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+}
