c617a80404d0006aac056a167ab90b6d1eb6a7fb
Hanul Kim <s2122@e-mirim.hs.kr>
2023-06-13 15:05:28 +0900

    model.addAttribute를 이용하여 result.html에 출력하기

--- src/main/java/kr/hs/study/Model2/controller/TestController.java
+++ src/main/java/kr/hs/study/Model2/controller/TestController.java
@@ -1,20 +1,35 @@
 package kr.hs.study.Model2.controller;
 
+import kr.hs.study.Model2.dto.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
-    // test1에 접속했을 때 result.html를 실행
+//    // test1에 접속했을 때 result.html를 실행
+//    @GetMapping("/test1")
+//    public String test1(@RequestParam("id") String id,
+//                        @RequestParam("pass") String pass, Model model) {
+//        System.out.println(id);
+//        System.out.println(pass);
+//        model.addAttribute("uid", id);
+//        model.addAttribute("upass", pass);
+//        return "result";
+//    }
+
+
     @GetMapping("/test1")
-    public String text1(@RequestParam("id") String id,
-                        @RequestParam("pass") String pass, Model model) {
-        System.out.println(id);
-        System.out.println(pass);
-        model.addAttribute("uid", id);
-        model.addAttribute("upass", pass);
+    public String test1(user u, Model model) {
+        String uid = u.getId();
+        String upass = u.getPass();
+        model.addAttribute("uid", uid);
+        model.addAttribute("upass", upass);
+
         return "result";
     }
+
+
 }
---
+++ src/main/java/kr/hs/study/Model2/dto/user.java
@@ -0,0 +1,9 @@
+package kr.hs.study.Model2.dto;
+
+import lombok.Data;
+
+@Data
+public class user {
+    private String id;
+    private String pass;
+}
