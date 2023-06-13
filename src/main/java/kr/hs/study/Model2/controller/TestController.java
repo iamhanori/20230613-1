cfcac74278f0ec1de7776c033766ad4861872455
Hanul Kim <s2122@e-mirim.hs.kr>
2023-06-13 14:51:02 +0900

    result.html에 id, pass 출력

--- src/main/java/kr/hs/study/Model2/controller/TestController.java
+++ src/main/java/kr/hs/study/Model2/controller/TestController.java
@@ -1,6 +1,7 @@
 package kr.hs.study.Model2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,10 +10,11 @@ public class TestController {
     // test1에 접속했을 때 result.html를 실행
     @GetMapping("/test1")
     public String text1(@RequestParam("id") String id,
-                        @RequestParam("pass") String pass) {
+                        @RequestParam("pass") String pass, Model model) {
         System.out.println(id);
         System.out.println(pass);
-
+        model.addAttribute("uid", id);
+        model.addAttribute("upass", pass);
         return "result";
     }
 }
